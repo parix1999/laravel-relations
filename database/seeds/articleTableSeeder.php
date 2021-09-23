525c96d766885574149f1a1c474fd3a578bf4356
parix1999 <parisottoedoardo@gmail.com>
2021-09-23 13:13:05 +0200

    seeder error

--- database/seeds/articleTableSeeder.php
+++ database/seeds/articleTableSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+// Importare i due model:
+use App\article; 
+use App\author;
+// Per il faker:
+use Faker\Generator as Faker;
 
 class articleTableSeeder extends Seeder
 {
@@ -9,7 +14,7 @@ class articleTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         // Author:
         $authorNameList = [
@@ -22,16 +27,45 @@ class articleTableSeeder extends Seeder
         ];
         $authorSurnameList = [
             'Paoli',
-            'Acquafresca',
-            'Salvio',
-            'Tornaindietro',
-            'Lucio',
-            'Dallio'
+            'Banfi',
+            'Ciano',
+            'Verdi',
+            'Arcolo',
+            'Patrizio'
         ];
 
-        $listAuthorId = [];
+        // $listAuthorId = [];
+       
+        for($i = 0; $i < 10; $i++) {
+            // Creazione dell'oggetto:
+            $authorObject = new author();
+
+            $randNameKey = array_rand($authorNameList, 1);
+            $randName = $authorNameList[$randNameKey];
+            $authorObject->name = $randName;
+
+            $randSurnameKey = array_rand($authorSurnameList, 1);
+            $randSurname = $authorSurnameList[$randSurnameKey];
+            $authorObject->surname = $randSurname;
+
+            $authorObject->email = $faker->email();
+            $authorObject->birth_year = $faker->year();
+            $authorObject->save();
+
+            // Creazione dell'oggetto:
+            $articleObject = new article();
+            $articleObject->title = $faker->words(5);
+            $articleObject->author_id = $articleObject->id;
+            $articleObject->descrizione = $faker->paragraph(3);
+            $articleObject->cover =  $faker->imageUrl(400, 400, 'article', true);
+            $articleObject->created_at = null;
+            $articleObject->updated_at = null;
+            $articleObject->save();
+        }
+
         
 
+        
 
 
     }
