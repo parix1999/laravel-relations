581d7599696738ec1881cb682b41456700737671
parix1999 <parisottoedoardo@gmail.com>
2021-09-23 15:48:59 +0200

    seeder error

--- database/seeds/articleTableSeeder.php
+++ database/seeds/articleTableSeeder.php
@@ -25,45 +25,39 @@ class articleTableSeeder extends Seeder
             'Gianpaolo',
             'Vercingitore'
         ];
-        $authorSurnameList = [
-            'Paoli',
-            'Banfi',
-            'Ciano',
-            'Verdi',
-            'Arcolo',
-            'Patrizio'
-        ];
-
-        // $listAuthorId = [];
-       
-        for($i = 0; $i < 10; $i++) {
+        $arrayIdAuthor = [];
+        
+        foreach ($authorNameList as $author) {
             // Creazione dell'oggetto:
             $authorObject = new author();
-
-            $randNameKey = array_rand($authorNameList, 1);
-            $randName = $authorNameList[$randNameKey];
-            $authorObject->name = $randName;
-
-            $randSurnameKey = array_rand($authorSurnameList, 1);
-            $randSurname = $authorSurnameList[$randSurnameKey];
-            $authorObject->surname = $randSurname;
-
+            // $randNameKey = array_rand($authorNameList, 1);
+            // $randName = $authorNameList[$randNameKey];
+            $authorObject->name = $author;
+            // $randSurnameKey = array_rand($authorSurnameList, 1);
+            // $randSurname = $authorSurnameList[$randSurnameKey];
+            $authorObject->surname = $faker->name(1);
             $authorObject->email = $faker->email();
             $authorObject->birth_year = $faker->year();
-            $authorObject->save();
 
+            $arrayIdAuthor[] = $authorObject->id; 
+            $authorObject->save();
+            
+        }
+         
+        for($i = 0; $i < 10; $i++) {
             // Creazione dell'oggetto:
             $articleObject = new article();
             $articleObject->title = $faker->words(5);
-            $articleObject->author_id = $articleObject->id;
+
+            $randIdKey = array_rand($arrayIdAuthor, 1);
+            $randId = $arrayIdAuthor[$randIdKey];
+            $articleObject->author_id = $randId; 
+        
             $articleObject->descrizione = $faker->paragraph(3);
             $articleObject->cover =  $faker->imageUrl(400, 400, 'article', true);
-            $articleObject->created_at = null;
-            $articleObject->updated_at = null;
+            
             $articleObject->save();
         }
-
-        
 
         
 
