362c0874e64e2d618d3102def73dfb53865e5689
parix1999 <parisottoedoardo@gmail.com>
2021-09-24 18:15:12 +0200

    grafic update

--- resources/views/paper/show.blade.php
+++ resources/views/paper/show.blade.php
@@ -12,15 +12,18 @@
     <div class="container">
         <h1>Articolo di  {{ $article->author->name }}  {{ $article->author->surname }} </h1>   
         <div class="row">
-            <div class="space col-3">
-                <div class="card">
-                    <div class="title">
-                        {{ $article->title }}
-                    </div>
-                    <div class="image">
-                        <img src="{{ $article->cover }}" alt="articolo {{ $article->title }}">
-                    </div>
+            <div class="space col-12">
+    
+                <div class="titleshow">
+                    <strong>{{ $article->title }}</strong>
                 </div>
+                <div class="image">
+                    <img src="{{ $article->cover }}" alt="articolo {{ $article->title }}">
+                </div>
+                <div class="testo-articolo">
+                    {{ $article->testo }}
+                </div>
+                
             </div>
         </div>
     </div>
